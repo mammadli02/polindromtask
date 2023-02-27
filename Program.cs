24d3c35c339ed753cf0c6d90fae979b4e558965f
mammadli02 <memmedliyazgul2002@gmail.com>
2023-02-27 21:55:35 +0400

    added new js file

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace Polindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number2; 
+            int number3; 
+            int result = 0;
+            int number;
+           
+            Console.WriteLine("ededi daxil edin : ");
+ number = Convert.ToInt32(Console.ReadLine());
+            number3 = number;
+            while (number > 0)
+            {
+                number2 = number % 10;
+
+                result = result * 10 + number2;
+                number = number / 10;
+
+            }
+            if (result == number3)
+            {
+                Console.WriteLine("polindrom ededdir");
+            }
+            else
+            {
+                Console.WriteLine( "polindrom deyil");
+            }
+
+        }
+    }
+}
